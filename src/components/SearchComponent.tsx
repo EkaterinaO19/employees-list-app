adcd6d399cf27d4d391b81d437041bf67d481335
EkaterinaO19 <ekshepa@gmail.com>
2023-07-04 19:40:12 +0700

    added profile func

--- src/components/SearchComponent.tsx
+++ src/components/SearchComponent.tsx
@@ -14,15 +14,16 @@ export default function SearchComponent() {
                     </svg>
                 </div>
                 <input
-                    type="search"
-                    id="default-search"
-                    className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 focus:outline-none rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
                     placeholder="Search name, tag, email..." required
                 />
-                <button
-                    type="submit"
-                    className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search
-                </button>
+                <div className={"absolute right-2.5 bottom-2.5 focus:outline-none px-4 py-2 cursor-pointer"}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
+                         stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round"
+                              d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25"/>
+                    </svg>
+                </div>
             </div>
         </form>
     )
